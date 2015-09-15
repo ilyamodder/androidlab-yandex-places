c1d41d23a8622eb8ae9b50bd4120f6fc8b0d8f5f
ilya <ilya@clienddev.ru>
2015-09-16 00:24:13 +0300

    Added auto zoom

--- app/src/main/java/com/googlemaps/template/myapplication/MainActivity.java
+++ app/src/main/java/com/googlemaps/template/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -157,6 +158,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     polylineOptions.add(point);
                 }
                 googleMap.addPolyline(polylineOptions);
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(location.getLatitude(), location.getLongitude()), 11));
             }
         });
     }
