c4734f173ba486658a2bbb411c48fd187df16b8a
ilya <ilya@clienddev.ru>
2015-09-17 17:57:12 +0300

    Added task2's base project

---
+++ task2/app/src/main/java/itis/practice/imagestask/MainActivity.java
@@ -0,0 +1,15 @@
+package itis.practice.imagestask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO : for image urls use Constants class or define your own if you want
+    }
+
+}
