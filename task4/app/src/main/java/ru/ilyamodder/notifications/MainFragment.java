f34ca4f18daaf9678a74e31dc0b810b6482135e1
ilya <ilya@clienddev.ru>
2015-09-23 07:18:28 +0300

    Added wifi notification

--- task4/app/src/main/java/ru/ilyamodder/notifications/MainFragment.java
+++ task4/app/src/main/java/ru/ilyamodder/notifications/MainFragment.java
@@ -5,19 +5,23 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -111,7 +115,20 @@ public class MainFragment extends Fragment {
     }
 
     private void showOnWifi() {
+        final EditText editText = new EditText(getActivity());
 
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Редактирование описания")
+                .setView(editText)
+                .setPositiveButton("Изменить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        PreferenceManager.getDefaultSharedPreferences(getActivity())
+                                .edit()
+                                .putString("ssid", editText.getText().toString())
+                                .commit();
+                    }
+                }).show();
     }
 
     private void showAfterOneMinute() {
--- task4/app/src/main/java/ru/ilyamodder/notifications/NotificationReceiver.java
+++ task4/app/src/main/java/ru/ilyamodder/notifications/NotificationReceiver.java
@@ -5,6 +5,10 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 
 public class NotificationReceiver extends BroadcastReceiver {
@@ -34,6 +38,26 @@ public class NotificationReceiver extends BroadcastReceiver {
                     .setOngoing(true)
                     .build();
             notifyMgr.notify(0, notification);
+        } else if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            if (networkInfo.isConnected()) {
+
+                WifiManager wifiManager = (WifiManager) context.getSystemService (Context.WIFI_SERVICE);
+                WifiInfo info = wifiManager.getConnectionInfo();
+                String ssid = info.getSSID();
+
+                String savedSsid = PreferenceManager.getDefaultSharedPreferences(context).getString("ssid", null);
+                if (savedSsid != null && savedSsid.equals(ssid)) {
+                    Notification notification = notificationBuilder
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .build();
+
+                    notifyMgr.notify(0, notification);
+                }
+            }
+
+
         }
     }
 }
